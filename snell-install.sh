873e84cb7b84075ce29948f1c2a0adccb0077831
easayliu <easay.liu@gmail.com>
2025-10-11 15:00:41 +0800

    feat: 修改支持v6监听

--- snell-install.sh
+++ snell-install.sh
@@ -78,7 +78,7 @@ SNELL_PSK=$(openssl rand -base64 32 | tr -d '=')
 # Create config file with specified port
 tee /etc/snell/snell-server.conf > /dev/null <<EOF
 [snell-server]
-listen = 0.0.0.0:$SNELL_PORT
+listen = :::$SNELL_PORT
 psk = $SNELL_PSK
 ipv6 = false
 EOF
